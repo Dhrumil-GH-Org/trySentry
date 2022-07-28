039c17085dd52e95388d62b97093f97ab6443d2a
Dhrumil <dhrumildparekh@gmail.com>
2022-07-28 14:18:05 -0700

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,31 @@
+import sentry_sdk
+import time
+
+sentry_sdk.init(
+    dsn="",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
+
+str='Test1'
+no=1
+
+while True:
+    no=no+1
+    u1=int(input("Enter commmand:"))
+    if u1==1:
+        time.sleep(3)
+        break
+    try:
+        str+no
+    except Exception as err:
+        sentry_sdk.capture_exception(err)
+    try:
+        b=1/0
+    except Exception as err:
+        sentry_sdk.capture_exception(err)
+
+    
