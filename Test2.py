4cebfa256aef1f2c03c385e64e845098a7c3f6f5
Dhrumil <dhrumil.parekh@sentry.io>
2022-08-03 10:01:00 -0700

    Commit16

---
+++ Test2.py
@@ -0,0 +1,28 @@
+import sentry_sdk
+import time
+
+sentry_sdk.init(
+    dsn="https://7752d499fda3425d873764302d362847@o1338503.ingest.sentry.io/6609457",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
+
+sen1=5
+sen2=2
+senNo=0
+while True:
+    senNo=senNo+1
+    print('Test1 ')
+    u1=int(input("Enter commmand:"))
+    if u1==1:
+        time.sleep(3)
+        break
+    try:
+        sen1+sen23
+    except Exception as err:
+        sentry_sdk.capture_exception(err)
+
+    
